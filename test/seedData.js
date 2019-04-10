042ed2c09518470573df5d0bdfe9981beaf11a22
Kristin Hortsch <kristinhortsch@gmail.com>
2019-04-09 17:41:28 -0700

    SEED DATA HAS SUCCESSFULLY BEEN LINKED TO RANDOM QUOTE API HUZZAHHHH

--- test/seedData.js
+++ test/seedData.js
@@ -1,15 +1,45 @@
 const Chirp = require('../lib/models/Chirp');
-const chance = require('chance').Chance();
+// const chance = require('chance').Chance();
+const { getQuote } = require('../lib/services/quotes');
 
 const seedData = () => {
   return Promise.all(
-    [...Array(100)].map(() => {
-      return Chirp.create({
-        chirp: chance.sentence(),
-        user: 'google-oauth2|102356666328752319799'
-      });
+    [...Array(10)].map(() => {
+      return getQuote()
+        .then(quote => {
+          return Chirp.create({
+            chirp: quote,
+            user: 'google-oauth2|102356666328752319799'
+          });
+        });
     })
+    
   );
 };
 
 module.exports = seedData;
+
+
+/*[...Array(10)].map(async() => {
+  getQuote().then(quote => {
+    return Chirp.create({
+      chirp: quote,
+      user: 'auth0|5cad2cd456fc520ebfaff30a'
+    });
+  })
+});
+*/
+
+/*
+return Promise.all(
+    [...Array(10)].map(() => {
+      getQuote()
+        .then(quote => {
+          return Chirp.create({
+            chirp: quote,
+            user: 'google-oauth2|102356666328752319799'
+          });
+        });
+    })
+  );
+*/
